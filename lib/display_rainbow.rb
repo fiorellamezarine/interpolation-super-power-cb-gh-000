d1ee065a358d1d5d7cc706a09833576a8fee1557
Fiorella Mezarine <nekili4@yahoo.com>
2018-03-17 22:52:30 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,4 @@
-def display_rainbow(colors)
-  
+colors = ['red', 'orange', 'yellow', 
+'green','blue','indigo', 'violet']
+def display_rainbow(me)
+  puts "R: #{me[0]}, O: #{me[1]}, Y: #{me[2]}, G: #{me[3]}, B: #{me
